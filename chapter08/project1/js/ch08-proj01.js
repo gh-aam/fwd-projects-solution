45eb6eef2f0fb7112feb9e98804062f9bdd129da
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-01-03 23:54:46 +0600

    File updated

--- chapter08/project1/js/ch08-proj01.js
+++ chapter08/project1/js/ch08-proj01.js
@@ -1,6 +1,40 @@
-
 const tax_rate = prompt('Enter tax rate (0.10)');
 const shipping_threshold = prompt('Enter shipping threshold (1000)');
+let subtotal = 0;
 
-/* add loop and other code here ... in this simple exercise we are not
-   going to concern ourselves with minimizing globals, etc */
+for (let c of cart) {
+    let total = calculateTotal(c.quantity, c.product.price);
+    outputCartRow(c, total);
+    subtotal += total;
+}
+
+let tax = subtotal * tax_rate;
+let shipping = 40.0;
+
+if (subtotal > shipping_threshold) {
+    shipping = 0.0;
+}
+
+let grand_total = subtotal + tax + shipping;
+
+/*
+document.write(`<tr class="totals">`);
+document.write(`<td colspan="4">Subtotal</td>`);
+document.write(`<td>$${subtotal.toFixed(2)}</td>`);
+document.write(`</tr>`);
+
+document.write(`<tr class="totals">`);
+document.write(`<td colspan="4">Tax</td>`);
+document.write(`<td>$${tax.toFixed(2)}</td>`);
+document.write(`</tr>`);
+
+document.write(`<tr class="totals">`);
+document.write(`<td colspan="4">Shipping</td>`);
+document.write(`<td>$${shipping.toFixed(2)}</td>`);
+document.write(`</tr>`);
+
+document.write(`<tr class="totals">`);
+document.write(`<td colspan="4" class="focus">Grand Total</td>`);
+document.write(`<td class="focus">$${grand_total.toFixed(2)}</td>`);
+document.write(`</tr>`);
+*/
--- chapter08/project1/js/functions.js
+++ chapter08/project1/js/functions.js
@@ -1,10 +1,13 @@
-/* define your functions here */
+function calculateTotal(quantity, price) {
+    return quantity * price;
+}
 
-
-
-
-
-
-
-
-        
+function outputCartRow(item, total) {
+    document.write(`<tr>`);
+    document.write(`<td><img src="../images/${item.product.filename}"></td>`);
+    document.write(`<td>${item.product.title}</td>`);
+    document.write(`<td>${item.quantity}</td>`);
+    document.write(`<td>$${item.product.price.toFixed(2)}</td>`);
+    document.write(`<td>$${total.toFixed(2)}</td>`);
+    document.write(`</tr>`);
+}
