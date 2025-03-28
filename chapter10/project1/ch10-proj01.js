2d9b85d34784d0d79b7165b9a74aefd50a948037
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-03-28 21:36:57 +0600

    File modified

--- chapter10/project1/ch10-proj01.js
+++ chapter10/project1/ch10-proj01.js
@@ -1,9 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-   const url = "https://www.randyconnolly.com/funwebdev/3rd/api/colors/sample-colors.php";
-   
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://www.randyconnolly.com/funwebdev/3rd/api/colors/sample-colors.php";
 });
-
-
-
- 
