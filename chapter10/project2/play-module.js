e072fa399c24ca764b72f00bbce7efbd95e5751c
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-04-26 01:36:17 +0600

    modified

--- chapter10/project2/play-module.js
+++ chapter10/project2/play-module.js
@@ -1,2 +1,21 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
+class Play {
+  constructor() {
+    
+  }
+}
+
+class Act {
+  constructor() {
+    
+  }
+}
+
+class Scene {
+  constructor() {
+    
+  }
+}
+
+export { Play, Act, Scene };
