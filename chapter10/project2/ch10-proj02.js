b2de1dbfd7a30bd2a44dff7b118a3fd0d13ed40d
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-04-27 21:02:15 +0600

    modified

--- chapter10/project2/ch10-proj02.js
+++ chapter10/project2/ch10-proj02.js
@@ -64,7 +64,9 @@ document.addEventListener("DOMContentLoaded", () => {
               
               if (firstScene) {
                 playHere.innerHTML = '';
-                displayScene(act, scene);
+                // displayScene(act, scene);
+                const display = new Play(playData, playHere, act, scene);
+                display.displayPlay();
                 firstScene = false;
               }
             });
@@ -72,11 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
             firstAct = false;
           }
         });
-        
-        // playHere.innerHTML = '';
-        // firstDisplayAfterFetch(data);
       } else {
-        // playHere.innerHTML = '';
         playHere.innerHTML = playHereBackup;
       }
     } catch (error) {
@@ -99,7 +97,9 @@ document.addEventListener("DOMContentLoaded", () => {
       
       if (firstScene) {
         playHere.innerHTML = '';
-        displayScene(selectedAct, scene);
+        // displayScene(selectedAct, scene);
+        const display = new Play(playData, playHere, selectedAct, scene);
+        display.displayPlay();
         firstScene = false;
       }
     });
@@ -110,75 +110,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const selectedScene = selectedAct.scenes.find(scene => scene.name === e.target.value);
     
     playHere.innerHTML = '';
-    displayScene(selectedAct, selectedScene);
+    // displayScene(selectedAct, selectedScene);
+    const display = new Play(playData, playHere, selectedAct, selectedScene);
+    display.displayPlay();
   });
   
   /*
-  function firstDisplayAfterFetch(playData) {
-    const h2 = document.createElement('h2');
-    h2.textContent = playData.title;
-    playHere.appendChild(h2);
-    
-    const article = document.createElement('article');
-    article.setAttribute('id', 'actHere');
-    
-    const h3 = document.createElement('h3');
-    h3.textContent = playData.acts[0].name;
-    article.appendChild(h3);
-    
-    const div1 = document.createElement('div');
-    div1.setAttribute('id', 'sceneHere');
-    
-    const currentScenes = playData.acts[0].scenes[0];
-    
-    const h4 = document.createElement('h4');
-    h4.textContent = currentScenes.name;
-    div1.appendChild(h4);
-    
-    const p1 = document.createElement('p');
-    p1.setAttribute('class', 'title');
-    p1.textContent = currentScenes.title;
-    div1.appendChild(p1);
-    
-    const p2 = document.createElement('p');
-    p2.setAttribute('class', 'direction');
-    p2.textContent = currentScenes.stageDirection;
-    div1.appendChild(p2);
-    
-    speechesFromScene1OfAct1(currentScenes, div1);
-    
-    article.appendChild(div1);
-    playHere.appendChild(article);
-  }
-  */
-  
-  /*
-  function speechesFromScene1OfAct1(currentScenes, div1) {
-    currentScenes.speeches.forEach(speech => {
-      const div2 = document.createElement('div');
-      div2.setAttribute('class', 'speech');
-      
-      const span = document.createElement('span');
-      span.textContent = speech.speaker;
-      div2.appendChild(span);
-      
-      speech.lines.forEach(line => {
-        const p3 = document.createElement('p');
-        p3.textContent = line;
-        div2.appendChild(p3);
-      });
-      
-      if (speech.stagedir) {
-        const em = document.createElement('em');
-        em.textContent = speech.stagedir;
-        div2.appendChild(em);
-      }
-      
-      div1.appendChild(div2);
-    });
-  }
-  */
-  
   function displayScene(act, scene) {
     const h2 = document.createElement('h2');
     h2.textContent = playData.title;
@@ -238,4 +175,5 @@ document.addEventListener("DOMContentLoaded", () => {
       div1.appendChild(div2);
     });
   }
+  */
 });
--- chapter10/project2/play-module.js
+++ chapter10/project2/play-module.js
@@ -1,20 +1,87 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
-class Play {
-  constructor() {
-    
-  }
-}
-
-class Act {
-  constructor() {
-    
-  }
-}
-
 class Scene {
-  constructor() {
+  constructor(scene) {
+    this.scene = scene;
+  }
+  
+  displaySpeeches(div1) {
+    this.scene.speeches.forEach(speech => {
+      const div2 = document.createElement('div');
+      div2.setAttribute('class', 'speech');
+      
+      const span = document.createElement('span');
+      span.textContent = speech.speaker;
+      div2.appendChild(span);
+      
+      speech.lines.forEach(line => {
+        const p3 = document.createElement('p');
+        p3.textContent = line;
+        div2.appendChild(p3);
+      });
+      
+      if (speech.stagedir) {
+        const em = document.createElement('em');
+        em.textContent = speech.stagedir;
+        div2.appendChild(em);
+      }
+      
+      div1.appendChild(div2);
+    });
+  }
+}
+
+class Act extends Scene {
+  constructor(playData, playHere, act, scene) {
+    super(scene);
+    this.playData = playData;
+    this.playHere = playHere;
+    this.act = act;
+  }
+  
+  displayScene() {
+    const h2 = document.createElement('h2');
+    h2.textContent = this.playData.title;
+    this.playHere.appendChild(h2);
     
+    const article = document.createElement('article');
+    article.setAttribute('id', 'actHere');
+    
+    const h3 = document.createElement('h3');
+    h3.textContent = this.act.name;
+    article.appendChild(h3);
+    
+    const div1 = document.createElement('div');
+    div1.setAttribute('id', 'sceneHere');
+    
+    const h4 = document.createElement('h4');
+    h4.textContent = this.scene.name;
+    div1.appendChild(h4);
+    
+    const p1 = document.createElement('p');
+    p1.setAttribute('class', 'title');
+    p1.textContent = this.scene.title;
+    div1.appendChild(p1);
+    
+    const p2 = document.createElement('p');
+    p2.setAttribute('class', 'direction');
+    p2.textContent = this.scene.stageDirection;
+    div1.appendChild(p2);
+    
+    super.displaySpeeches(div1);
+    
+    article.appendChild(div1);
+    this.playHere.appendChild(article);
+  }
+}
+
+class Play extends Act {
+  constructor(playData, playHere, act, scene) {
+    super(playData, playHere, act, scene);
+  }
+  
+  displayPlay() {
+    super.displayScene();
   }
 }
 
